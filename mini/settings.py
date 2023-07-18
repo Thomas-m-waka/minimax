2623f16f1d9b2acc76b6f4bacfc18676ebf6f7b5
Thomas-m-waka <thomasmmuteti4@gmail.com>
2023-07-18 10:47:42 +0300

    c

--- mini/settings.py
+++ mini/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['*']
 
 
 
